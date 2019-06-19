e43ef812bcbd6c7e81c2e3d555064c7c7152ebc7
Jan Galinski <jan.galinski@holisticon.de>
2019-06-19 16:51:23 +0200

    init

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    kotlin("jvm") version "1.3.21"
+
+    application
+}
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+
+    // Use the Kotlin test library.
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+
+    // Use the Kotlin JUnit integration.
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+}
+
+application {
+    // Define the main class for the application.
+    mainClassName = "com.github.jangalinski.dedomenon.AppKt"
+}
